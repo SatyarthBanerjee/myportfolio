15bd014e77bf60ca055387d2c264bdb5b916b80c
SatyarthBanerjee <satyarthbanerjee12@gmail.com>
2024-01-04 15:53:56 +0530

    responsiveness and hamburger menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Home'
 import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
+import Notfound from './Notfound';
 const App = () => {
   return (
     <>
@@ -13,6 +14,7 @@ const App = () => {
           <Route path ="/about" element={<About />}/>
           <Route path ="/projects" element={<Projects />}/>
           <Route path ="/contact" element={<Contact />}/>
+          <Route path ="*" element={<Notfound/>}/>
           
         </Routes>
       </Router>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,33 +1,63 @@
-import React from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const handleNav = (path)=>{
-    navigate(path)
-  }
+  const handleNav = (path) => {
+    navigate(path);
+  };
+  const [hamnav, sethamnav] = useState(false);
+  const [active, setactive] = useState(false);
+  const handlehamnav = () => {
+    sethamnav(!hamnav);
+    setactive(!active)
+  };
+  
   return (
-    <div className='navbar'>
-      <div className='lnav'>
-        <h1 onClick={()=>handleNav("/")}>Satyarth</h1>
+    <div className="navbar">
+      <div className="lnav">
+        <h1 onClick={() => handleNav("/")}>Satyarth</h1>
       </div>
-      <div className='rnav'>
+      <div className="rnav">
         <ul>
-            <li><NavLink  activeClassName="active" to="/">Home</NavLink></li>
-            <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
-            <li><NavLink activeClassName="active" to="/projects">Projects</NavLink></li>
-            <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
-            <div className='hamburger'>
-            <div className='hamnav 1'></div>
-            <div className='hamnav 2'></div>
-            <div className='hamnav 3'></div>
-            </div>
-          
+          <li>
+            <NavLink activeClassName="active" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/about">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/projects">
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/contact">
+              Contact
+            </NavLink>
+          </li>
         </ul>
-     
+        <div onClick={handlehamnav} className={`hamburger ${active?"lowgap":""}`}>
+          <div className={`hamnav ${active?"one":""} `}></div>
+          <div className={`hamnav ${active?"two":""} `}></div>
+          <div className={`hamnav ${active?"three":""} `}></div>
+        </div>
+      </div>
+      <div className={`${hamnav ? "hamburgermenu" : "closeham"}`}>
+        <ul>
+          <li><NavLink activeClassName="active" to="/">Home</NavLink></li>
+          <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+          <li><NavLink activeClassName="active" to="/projects">Projects</NavLink></li>
+          <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
+         
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
